615ab48a8210a0c6d680f753ac4888470e10cbf7
Sam Cooler <scooler@gmail.com>
2016-08-08 16:29:56 -0500

    add rig A initial config

---
+++ src/main/matlab/+sa_labs/+rigs/SchwartzLab_Rig_A.m
@@ -0,0 +1,28 @@
+classdef SchwartzLab_Rig_A < symphonyui.core.descriptions.RigDescription
+    
+    methods
+        
+        function obj = SchwartzLab_Rig_A()
+            import symphonyui.builtin.daqs.*;
+            import symphonyui.builtin.devices.*;
+            import symphonyui.core.*;
+            
+            daq = HekaDaqController(HekaDeviceType.USB18);
+            obj.daqController = daq;
+            
+%             amp1 = MultiClampDevice('Amp1', 1, 834077).bindStream(daq.getStream('ANALOG_OUT.0')).bindStream(daq.getStream('ANALOG_IN.0'));
+%             obj.addDevice(amp1);
+%             
+%             amp2 = MultiClampDevice('Amp2', 2, 834077).bindStream(daq.getStream('ANALOG_OUT.1')).bindStream(daq.getStream('ANALOG_IN.1'));
+%             obj.addDevice(amp2);
+%             
+            
+%             lightCrafter = sa_labs.devices.LightCrafterDevice();
+%             lightCrafter.addConfigurationSetting('micronsPerPixel', 1.6, 'isReadOnly', true);
+%             obj.addDevice(lightCrafter);
+        end
+        
+    end
+    
+end
+
