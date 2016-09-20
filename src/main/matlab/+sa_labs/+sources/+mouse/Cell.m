e7e87f219145fc49f7982f14b4b428a8755342d9
Sam Cooler <scooler@gmail.com>
2016-09-20 10:26:36 -0500

    add property type for cell number

--- src/main/matlab/+sa_labs/+sources/+mouse/Cell.m
+++ src/main/matlab/+sa_labs/+sources/+mouse/Cell.m
@@ -6,9 +6,12 @@ classdef Cell < symphonyui.core.persistent.descriptions.SourceDescription
             import symphonyui.core.*;
             
             obj.addProperty('number',int32(1),...
-                'description', 'The number of the cell')
+                'description', 'The number of the cell (just the number)',...
+                PropertyType('int32', 'scalar'))
+            
             obj.addProperty('type', '', ...
                 'description', 'The type of the recorded cell');
+            
             obj.addProperty('location',[0,0],...
                 'description', 'coordinates of the cell on the retina');
             
