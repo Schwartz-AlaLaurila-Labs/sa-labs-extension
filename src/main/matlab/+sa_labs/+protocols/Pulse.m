ab2db83ec243c7c2b673c487943452dadc804194
Sam Cooler <scooler@gmail.com>
2017-01-24 14:53:26 -0600

    add initial pulse protocol

---
+++ src/main/matlab/+sa_labs/+protocols/Pulse.m
@@ -0,0 +1,68 @@
+classdef Pulse < sa_labs.protocols.BaseProtocol
+    % Presents a set of rectangular pulse stimuli to a specified amplifier and records from the same amplifier.
+    % from the rieke lab with our thanks
+    
+    properties
+        outputAmpSelection              % Output amplifier (1 or 2)
+        preTime = 50                    % Pulse leading duration (ms)
+        stimTime = 500                  % Pulse duration (ms)
+        tailTime = 1000                   % Pulse trailing duration (ms)
+        pulseAmplitude = 100            % Pulse amplitude (mV or pA depending on amp mode)
+        numberOfEpochs = 50;
+    end
+    
+    properties (Hidden)
+        responsePlotMode = 'cartesian';
+        responsePlotSplitParameter = '';
+    end
+    
+    properties (Hidden, Dependent)
+        totalNumEpochs
+    end    
+    
+    methods
+                
+        
+        function prepareRun(obj)
+            prepareRun@edu.washington.riekelab.protocols.RiekeLabProtocol(obj);
+            
+        end
+        
+        function stim = createAmpStimulus(obj, ampName)
+            gen = symphonyui.builtin.stimuli.PulseGenerator();
+            
+            gen.preTime = obj.preTime;
+            gen.stimTime = obj.stimTime;
+            gen.tailTime = obj.tailTime;
+            gen.amplitude = obj.pulseAmplitude;
+            gen.mean = obj.rig.getDevice(ampName).background.quantity;
+            gen.sampleRate = obj.sampleRate;
+            gen.units = obj.rig.getDevice(ampName).background.displayUnits;
+            
+            stim = gen.generate();
+        end
+                
+        function prepareEpoch(obj, epoch)
+            prepareEpoch@sa_labs.protocols.BaseProtocol(obj, epoch);
+            
+            devices = obj.rig.getInputDevices();
+            for i = 1:numel(devices)
+                if epoch.hasResponse(devices{i})
+                    epoch.removeResponse(devices{i});
+                end
+            end
+            
+            outputAmpName = sprintf('amp%g', obj.outputAmpSelection);
+            epoch.addStimulus(obj.rig.getDevice(outputAmpName), obj.createAmpStimulus(outputAmpName));
+        end
+        
+        
+        function totalNumEpochs = get.totalNumEpochs(obj)
+            totalNumEpochs = obj.numberOfEpochs;
+        end        
+
+        
+    end
+    
+end
+
