d69afb4fc168413f8211a433db7a23180f127db9
Sam Cooler <scooler@gmail.com>
2017-03-02 16:27:39 -0600

    update test rig description

--- src/main/matlab/+sa_labs/+rigs/TestRig_SchwartzLab.m
+++ src/main/matlab/+sa_labs/+rigs/TestRig_SchwartzLab.m
@@ -4,22 +4,21 @@ classdef TestRig_SchwartzLab < sa_labs.rigs.SchwartzLab_Rig_Base
         % properties not accessible here; have to be fed into a device to work
         rigName = 'Test Rig';
         testMode = true;
-        filterWheelValidPositions = [2, 4, 5, 6, 7, 8];
+        filterWheelNdfValues = [2, 4, 5, 6, 7, 8];
+        filterWheelDefaultValue = 5;
         filterWheelAttenuationValues = [0.0105, 8.0057e-05, 6.5631e-06, 5.5485e-07, 5.5485e-08, 5.5485e-09];
-
+        filterWheelComPort = -1; % 
+        numberOfAmplifiers = 2;
         
-        NTCfitBlue = [-5.093e-10, 2.899e-07, -2.697e-06]; %added 04/01/16 by Todd -- fit coeff. for non tricolor stimuli on upper projector 6/16 edit
-        NTCfitGreen = [-5.266e-11, 3.749e-08, 4.664e-07]; %added 04/01/16 by Todd -- fit coeff. for non tricolor stimuli on upper projector
-        NTCfitUV = [-3.593e-12, 5.752e-09, 5.097e-07]; %added 04/01/16 by Todd -- fit coeff. for non tricolor stimuli on upper projector
-        fitBlue = [-1.566e-10, 1.317e-07, -9.457e-07]; %added 3/21/16 by Todd for TriColor stims
-        fitGreen = [-1.931e-11, 1.278e-08,  1.389e-07]; %added 3/21/16 by Todd for TriColor stims
-        fitUV = [-2.346e-12, 1.883e-09,1.58e-07]; %added 3/21/16 by Todd for TriColor stims        
+        fitBlue = [-1.566e-10, 1.317e-07, -9.457e-07];
+        fitGreen = [-1.931e-11, 1.278e-08,  1.389e-07];
+        fitUV = [];
         
-        micronsPerPixel = 1.6;
+        micronsPerPixel = 1.38;
         projectorAngleOffset = 180;
         frameTrackerPosition = [20,20];
         frameTrackerSize = [40,40];
-        filterWheelComPort = nan;
+        
         
         projectorColorMode = 'standard';
     end
