11063a0ec34df19ade35718b2bbc4a35128f8695
Sathish Narayanan <ragav.sathish@gmail.com>
2017-07-25 15:46:37 +0300

    Provide support for cell label based on number of Amps

--- src/main/matlab/+sa_labs/+sources/+mouse/Cell.m
+++ src/main/matlab/+sa_labs/+sources/+mouse/Cell.m
@@ -4,19 +4,23 @@ classdef Cell < symphonyui.core.persistent.descriptions.SourceDescription
         
         function obj = Cell()
             import symphonyui.core.*;
+            numberOfChannels = sa_labs.factory.getInstance('rigProperty').numberOfChannels;
             
-            obj.addProperty('number',1,...
-                'description', 'The number of the cell (just the number)')
-            
-            obj.addProperty('type', '', ...
-                'description', 'The guessed type of the recorded cell');
-            
-            obj.addProperty('confirmedType', '', ...
-                'description', 'The confirmed type of the recorded cell');            
-            
-            obj.addProperty('location',[0,0],...
-                'description', 'coordinates of the cell on the retina');
-            
+            for i = 1 : numberOfChannels
+                propName = @(str) strcat('Amp', num2str(i), str);
+                category = propName('');
+                obj.addProperty(propName('CellType'), '', ...
+                    'description', 'The guessed type of the recorded cell',...
+                    'category', category);
+
+                obj.addProperty(propName('ConfirmedCellType'), '', ...
+                    'description', 'The confirmed type of the recorded cell',...
+                    'category', category);            
+
+                obj.addProperty(propName('Location'),[0,0],...
+                    'description', 'coordinates of the cell on the retina',...
+                    'category', category);
+            end
             obj.addAllowableParentType('sa_labs.sources.Mouse');
         end
         
