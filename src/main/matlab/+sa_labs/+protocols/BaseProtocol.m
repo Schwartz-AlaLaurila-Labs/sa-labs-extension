f6d525f435ad4a23d2d05541291c284f8b87d543
Sathish Narayanan <ragav.sathish@gmail.com>
2017-07-25 18:22:31 +0300

    Enable response figure(s) for amplifier channel(s)

--- src/main/matlab/+sa_labs/+protocols/BaseProtocol.m
+++ src/main/matlab/+sa_labs/+protocols/BaseProtocol.m
@@ -1,31 +1,26 @@
 classdef (Abstract) BaseProtocol < symphonyui.core.Protocol
-% this class handles protocol control which is not visual stimulus specific
-
+    
+    % This class handles protocol control which is not visual stimulus specific
+    
     properties
         chan1 = 'Amp1';
         chan1Mode = 'Cell attached'
         chan1Hold = 0
-        
         chan2 = 'None';
         chan2Mode = 'Off'
         chan2Hold = 0
-        
         chan3  = 'None';
         chan3Mode = 'Off'
         chan3Hold = 0
-        
         chan4  = 'None';
         chan4Mode = 'Off'
         chan4Hold = 0
-        
         spikeDetectorMode = 'advanced';
         spikeThreshold = -6 % pA or (pseudo-)std
     end
     
     properties (Transient, Hidden)
         responseFigure
-        devices % For figures
-
     end
     
     properties (Abstract)
@@ -44,8 +39,6 @@ classdef (Abstract) BaseProtocol < symphonyui.core.Protocol
         chan2ModeType = symphonyui.core.PropertyType('char', 'row', {'Cell attached','Whole cell','Off'});
         chan3ModeType = symphonyui.core.PropertyType('char', 'row', {'Cell attached','Whole cell','Off'});
         chan4ModeType = symphonyui.core.PropertyType('char', 'row', {'Cell attached','Whole cell','Off'});
-        ampList
-        
         spikeDetectorModeType = symphonyui.core.PropertyType('char', 'row', {'advanced', 'Simple Threshold', 'Filtered Threshold', 'none'});
     end
     
@@ -54,18 +47,25 @@ classdef (Abstract) BaseProtocol < symphonyui.core.Protocol
         function didSetRig(obj)
             didSetRig@symphonyui.core.Protocol(obj);
             
-            obj.ampList = horzcat({'None'}, obj.rig.getDeviceNames('Amp'));
+            ampDeviceNames = obj.rig.getDeviceNames('Amp');
             
-            obj.chan1Type = symphonyui.core.PropertyType('char', 'row', obj.ampList(2:end)); % first channel should always be filled
-            obj.chan2Type = symphonyui.core.PropertyType('char', 'row', obj.ampList);
-            obj.chan3Type = symphonyui.core.PropertyType('char', 'row', obj.ampList);
-            obj.chan4Type = symphonyui.core.PropertyType('char', 'row', obj.ampList);
-            
+            % Initialize chanTypes with 'None' & Amp device name if exist
+            % Like {'None, Amp1', 'None, Amp2'} .. etc
 
+            channelTypes = cell(1, 4);
+            channelTypes(:) = {'None'};
+            for i = 1 : numel(ampDeviceNames)
+                channelTypes{i} = strcat(channelTypes{i}, ',', ampDeviceNames{i});
+            end
+            
+            obj.chan1Type = symphonyui.core.PropertyType('char', 'row', strsplit(channelTypes{1}, ','));
+            obj.chan2Type = symphonyui.core.PropertyType('char', 'row', strsplit(channelTypes{2}, ','));
+            obj.chan3Type = symphonyui.core.PropertyType('char', 'row', strsplit(channelTypes{3}, ','));
+            obj.chan4Type = symphonyui.core.PropertyType('char', 'row', strsplit(channelTypes{4}, ','));
         end
-
+        
         function d = getPropertyDescriptor(obj, name)
-            d = getPropertyDescriptor@symphonyui.core.Protocol(obj, name);        
+            d = getPropertyDescriptor@symphonyui.core.Protocol(obj, name);
             
             switch name
                 case {'numberOfCycles','numberOfEpochs','ndfs'}
@@ -80,68 +80,42 @@ classdef (Abstract) BaseProtocol < symphonyui.core.Protocol
             
             if strncmp(name, 'chan', 4)
                 d.category = '9 Amplifiers';
-                if str2double(name(5)) > (length(obj.ampList) - 1)
-                    d.isHidden = true;
-                end
+                dependent = getPropertyDescriptor@symphonyui.core.Protocol(obj, name(1:5));
+                d.isHidden =  numel(dependent.type.domain) < 2;
             end
-        end 
-        
+        end
         
         function prepareRun(obj, setAmpHoldSignals)
             prepareRun@symphonyui.core.Protocol(obj);
-
-            % TODO: check that two channels don't use the same amp (makes settings collision)
-
-%             Set amp hold signals.
-            if nargin < 2
-                setAmpHoldSignals = true;
+            
+            if nargin < 2 || setAmpHoldSignals
+                obj.applyBackground();
             end
-            if setAmpHoldSignals
-                for ci = 1:4
-                    channelName = sprintf('chan%d', ci);
-    %                 modeName = sprintf('chan%dMode', ci);
-                    holdName = sprintf('chan%dHold', ci);
-                    newBackground = obj.(holdName);
-
-                    if strcmp(obj.(channelName),'None')
-                        continue
-                    end
-                    ampName = obj.(channelName);
-                    device = obj.rig.getDevice(ampName);
-                    prevBackground = device.background;
-
-                    device.background = symphonyui.core.Measurement(newBackground, device.background.displayUnits);
-                    device.applyBackground();
-                    
-                    if prevBackground ~= newBackground
-                        pause(5);
-                    end
-                end
-            end
-
+            
             % make device list for analysis figure
-            obj.devices = {};
-            for ci = 1:4
-                ampName = obj.(['chan' num2str(ci)]);
-                ampMode = obj.(['chan' num2str(ci) 'Mode']);
-                if ~(strcmp(ampName, 'None') || strcmp(ampMode, 'Off'));
-                    device = obj.rig.getDevice(ampName);
-                    obj.devices{end+1} = device; %#ok<AGROW>
+            if obj.isPlotEnabled()
+                for i = 1 : 4
+                    channelProperty = strcat('chan', num2str(i));
+                    if obj.isChannelActive(channelProperty)
+                        device = obj.rig.getDevice(obj.(channelProperty));
+                        class = strcat('sa_labs.figures.ResponseAnalysisFigure', num2str(i));
+                        obj.createResponseFigure(class, {device}, obj.([channelProperty 'Mode']));
+                    end
                 end
             end
+        end
+        
+        function createResponseFigure(obj, class, device, mode)
             
-            if obj.responsePlotMode ~= false
-                obj.responseFigure = obj.showFigure('sa_labs.figures.ResponseAnalysisFigure', obj.devices, ...
-                    'activeFunctionNames', {'mean'}, ...
-                    'totalNumEpochs',obj.totalNumEpochs,...
-                    'epochSplitParameter',obj.responsePlotSplitParameter,...
-                    'plotMode',obj.responsePlotMode,... 
-                    'analysisRegion', 1e-3 * [obj.preTime, obj.preTime + obj.stimTime + 0.5],...
-                    'responseMode',obj.chan1Mode,... % TODO: different modes for multiple amps
-                    'spikeThreshold', obj.spikeThreshold, ...
-                    'spikeDetectorMode', obj.spikeDetectorMode);
-            end
-            
+            obj.responseFigure = obj.showFigure(class, device, ...
+                'activeFunctionNames', {'mean'}, ...
+                'totalNumEpochs', obj.totalNumEpochs,...
+                'epochSplitParameter', obj.responsePlotSplitParameter,...
+                'plotMode', obj.responsePlotMode,...
+                'analysisRegion', 1e-3 * [obj.preTime, obj.preTime + obj.stimTime + 0.5],...
+                'responseMode', mode,...
+                'spikeThreshold', obj.spikeThreshold, ...
+                'spikeDetectorMode', obj.spikeDetectorMode);
         end
         
         function prepareEpoch(obj, epoch)
@@ -152,17 +126,13 @@ classdef (Abstract) BaseProtocol < symphonyui.core.Protocol
                 epoch.addParameter('protocolVersion', obj.version);
             end
             
-            for ci = 1:4
-                ampName = obj.(['chan' num2str(ci)]);
-                ampMode = obj.(['chan' num2str(ci) 'Mode']);
-                
-                if strcmp(ampName, 'None') || strcmp(ampMode, 'Off')
-                   continue
+            for i = 1:4
+                channelProperty = strcat('chan', num2str(i));
+                if obj.isChannelActive(channelProperty)
+                    ampDevice = obj.rig.getDevice(obj.(channelProperty));
+                    epoch.addResponse(ampDevice);
                 end
-                ampDevice = obj.rig.getDevice(ampName);
-                epoch.addResponse(ampDevice);
             end
-                                    
         end
         
         function tf = shouldContinuePreparingEpochs(obj)
@@ -173,11 +143,55 @@ classdef (Abstract) BaseProtocol < symphonyui.core.Protocol
             tf = obj.numEpochsCompleted < obj.totalNumEpochs;
         end
         
+        function tf= isChannelActive(obj, channelProperty)
+            ampName = obj.(channelProperty);
+            ampMode = obj.([channelProperty 'Mode']);
+            tf = ~ (strcmp(ampName, 'None') || strcmp(ampMode, 'Off'));
+        end
+        
+        function tf = isPlotEnabled(obj)
+            if islogical(obj.responsePlotMode)
+                tf = obj.responsePlotMode;
+            else
+                tf = ~ strcmpi(obj.responsePlotMode, 'false');
+            end
+        end
+    end
+    
+    methods (Access = private)
+        
+        function applyBackground(obj)
+            for ci = 1:4
+                channelName = sprintf('chan%d', ci);
+                holdName = sprintf('chan%dHold', ci);
+                newBackground = obj.(holdName);
+                
+                if strcmp(obj.(channelName), 'None')
+                    continue
+                end
+                ampName = obj.(channelName);
+                device = obj.rig.getDevice(ampName);
+                prevBackground = device.background;
+                
+                device.background = symphonyui.core.Measurement(newBackground, device.background.displayUnits);
+                device.applyBackground();
+                
+                if prevBackground.quantity ~= newBackground
+                    pause(5);
+                end
+            end
+        end
+    end
+    
+    methods
+        
+        % Only used in test rig
+        
         function addGaussianLoopbackSignals(obj, epoch)
             % make fake input data via loopback
             for ci = 1:4
                 if strcmp(obj.(['chan' num2str(ci)]), 'None')
-                   continue
+                    continue
                 end
                 device = obj.rig.getDevice(obj.(['chan' num2str(ci)]));
                 g = sa_labs.stimuli.GaussianNoiseGeneratorV2();
@@ -193,9 +207,8 @@ classdef (Abstract) BaseProtocol < symphonyui.core.Protocol
                 g.sampleRate = obj.sampleRate;
                 epoch.addStimulus(device, g.generate());
             end
-
         end
-
+        
     end
     
 end
