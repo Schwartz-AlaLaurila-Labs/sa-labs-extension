4e787f280d6bcb1fb5e857052fcac920937b9a13
Sathish Narayanan <ragav.sathish@gmail.com>
2017-07-26 14:42:04 +0300

    One to One Mapping of Chan and Amp configuration

--- src/main/matlab/+sa_labs/+modules/CommonControl.m
+++ src/main/matlab/+sa_labs/+modules/CommonControl.m
@@ -36,8 +36,7 @@ classdef CommonControl < symphonyui.ui.Module
         color = 'cyan'       
         colorType = symphonyui.core.PropertyType('char', 'row', {'cyan','blue','green'});
         
-        projectorPropertyNames = {'spikeThreshold','spikeDetectorMode','color','NDF','frameRate','patternRate','blueLED','greenLED','offsetX','offsetY'};
-        ampList
+        projectorPropertyNames = {'spikeThreshold','spikeDetectorMode','color','NDF', 'blueLED','greenLED','offsetX','offsetY'};
         
         chan1Type
         chan2Type
@@ -47,17 +46,11 @@ classdef CommonControl < symphonyui.ui.Module
         chan2ModeType = symphonyui.core.PropertyType('char', 'row', {'Cell attached','Whole cell','Off'});
         chan3ModeType = symphonyui.core.PropertyType('char', 'row', {'Cell attached','Whole cell','Off'});
         chan4ModeType = symphonyui.core.PropertyType('char', 'row', {'Cell attached','Whole cell','Off'});
-        
+  
         spikeDetectorModeType = symphonyui.core.PropertyType('char', 'row', {'Simple Threshold', 'Filtered Threshold', 'none'});       
-    
         protocolPropertyGrid
+        ampList
     end
-    
-%     methods (Access = protected)
-%         function willGo(obj)
-%             obj.updateDeviceList();
-%         end   
-%     end    
     
     methods (Access = protected)
         
@@ -82,17 +75,26 @@ classdef CommonControl < symphonyui.ui.Module
         end
 
         function updateDeviceList(obj)
+            
             devices = obj.configurationService.getDevices('Amp');
             obj.ampList = {};
             for i = 1:length(devices)
                 obj.ampList{i} = devices{i}.name;
             end
-            obj.ampList = horzcat({'None'}, obj.ampList);
             
-            obj.chan1Type = symphonyui.core.PropertyType('char', 'row', obj.ampList(2:end)); % first channel should always be filled
-            obj.chan2Type = symphonyui.core.PropertyType('char', 'row', obj.ampList);
-            obj.chan3Type = symphonyui.core.PropertyType('char', 'row', obj.ampList);
-            obj.chan4Type = symphonyui.core.PropertyType('char', 'row', obj.ampList);
+            % Initialize chanTypes with 'None' & Amp device name if exist
+            % Like {'None, Amp1', 'None, Amp2'} .. etc
+            
+            channelTypes = cell(1, 4);
+            channelTypes(:) = {'None'};
+            for i = 1 : numel(obj.ampList)
+                channelTypes{i} = strcat(channelTypes{i}, ',', obj.ampList{i});
+            end
+            
+            obj.chan1Type = symphonyui.core.PropertyType('char', 'row', strsplit(channelTypes{1}, ','));
+            obj.chan2Type = symphonyui.core.PropertyType('char', 'row', strsplit(channelTypes{2}, ','));
+            obj.chan3Type = symphonyui.core.PropertyType('char', 'row', strsplit(channelTypes{3}, ','));
+            obj.chan4Type = symphonyui.core.PropertyType('char', 'row', strsplit(channelTypes{4}, ','));
         end
         
         function createUi(obj, figureHandle)
@@ -113,11 +115,7 @@ classdef CommonControl < symphonyui.ui.Module
                 'Callback', @obj.cbSetParameters);
             
             set(layout, 'Heights', [-1, 30]);
-            
-%             obj.ampList = {'None','Amp1','Amp2','Amp3','Amp4'};
-            obj.populateProtocolProperties();
         end
-
         
         function cbSetParameters(obj, ~, ~)
             % set values in the protocol (callback on change settings in this module)
@@ -132,46 +130,35 @@ classdef CommonControl < symphonyui.ui.Module
         end
         
         function populateProtocolProperties(obj)
-            numAmps = length(obj.ampList) - 1;
-            % get values from the protocol to instantiate display
-            names = properties(obj);
-            excludeParam = zeros(size(names));
-            for i = 1:length(names)
-                
-                % Select out projector properties
-                if any(strcmp(names{i}, obj.projectorPropertyNames))
-                    continue
-                end
-                
-                %select out the channel parameters
-                if isempty(strfind(names{i}, 'chan')) % get rid of 
-                    excludeParam(i) = 1;
-                else
-                    % toss the ones with too high a channel number
-                    if numAmps < str2double(names{i}(5))
-                        excludeParam(i) = 1;
-                    end
-                end
-            end
-            names(logical(excludeParam)) = [];
-            % preceding 10 lines in 1 python line:
-            % names = [n for n in properties(obj) if not strfind(n, 'chan')]
             
-            % set the display category
-            descriptors = symphonyui.core.PropertyDescriptor.empty(0, numel(names));
-            for i = 1:numel(names)
-                descriptors(i) = symphonyui.core.PropertyDescriptor.fromProperty(obj, names{i});
-                if strfind(names{i}, 'chan')
-                    descriptors(i).category = sprintf('Channel %s',names{i}(5));
-                else
-                    descriptors(i).category = 'Projector';
-                end
-            end
+            numberOfActiveChannels = numel(obj.ampList);
 
+            % usage:  ampParamGen('chan', 'Hold') returns chan1Hold,
+            % chan2Hold if there exist two amplifier configuration
+            
+            ampParamGen = @(preFix, postFix) arrayfun(@(i) strcat(preFix, num2str(i), postFix), 1 : numberOfActiveChannels, 'UniformOutput', false);
+            ampPropertynames = [ampParamGen('chan', ''), ampParamGen('chan', 'Hold'), ampParamGen('chan', 'Mode')]; 
+            
+            index = 0;
+            n = numel(ampPropertynames) + numel(obj.projectorPropertyNames);
+            descriptors = symphonyui.core.PropertyDescriptor.empty(0, n);
+            
+            % set amplifer specific properties
+            for i = 1 : numel(ampPropertynames)
+                descriptors(i) = symphonyui.core.PropertyDescriptor.fromProperty(obj, ampPropertynames{i});
+                descriptors(i).category = sprintf('Channel %s',ampPropertynames{i}(5));
+                index = i;
+            end
+            
+            % set projector specific properties
+            for i = 1 : numel(obj.projectorPropertyNames)
+                descriptors(index + i) = symphonyui.core.PropertyDescriptor.fromProperty(obj, obj.projectorPropertyNames{i});
+                descriptors(index + i).category = 'Projector';
+            end
+            
             fields = symphonyui.ui.util.desc2field(descriptors);
             set(obj.protocolPropertyGrid, 'Properties', fields);
         end
-
     end
     
 end
