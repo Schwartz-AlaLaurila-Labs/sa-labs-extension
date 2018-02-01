1508bce89e0f32b391f9a3e56ddb613c8d2bb803
Sathish Kumar Narayanan <ragav.sathish@gmail.com>
2018-02-01 19:49:37 +0200

    Add bath temprature as epoch parameter

--- src/main/matlab/+sa_labs/+protocols/BaseProtocol.m
+++ src/main/matlab/+sa_labs/+protocols/BaseProtocol.m
@@ -135,7 +135,17 @@ classdef (Abstract) BaseProtocol < symphonyui.core.Protocol
                     epoch.addResponse(ampDevice);
                 end
             end
+            
+            controllers = obj.rig.getDevices('Temperature Controller');
+            if ~isempty(controllers)
+                epoch.addResponse(controllers{1});
+            end
         end
+        
+         function completeEpoch(obj, epoch)
+            completeEpoch@symphonyui.core.Protocol(obj, epoch);
+            obj.addBathTempratureToEpoch(epoch)
+         end
         
         function completeRun(obj)
             completeRun@symphonyui.core.Protocol(obj);
@@ -189,6 +199,25 @@ classdef (Abstract) BaseProtocol < symphonyui.core.Protocol
         function tf = hasValidPersistor(obj)
             p = obj.persistor;
             tf =  ~ isempty(p) && ~ isempty(p.currentEpochBlock);
+        end
+        
+        function addBathTempratureToEpoch(obj, epoch)
+            
+            controllers = obj.rig.getDevices('Temperature Controller');
+            if ~isempty(controllers) && epoch.hasResponse(controllers{1})
+                response = epoch.getResponse(controllers{1});
+                [quantities, units] = response.getData();
+                if ~strcmp(units, 'V')
+                    error('Temperature Controller must be in volts');
+                end
+                
+                % Temperature readout from Warner TC-324C controller 100 mV/degree C.
+                temperature = mean(quantities) * 1000 * (1/100);
+                temperature = round(temperature * 10) / 10;
+                epoch.addProperty('bathTemperature', temperature);
+                
+                epoch.removeResponse(controllers{1});
+            end
         end
     end
     
