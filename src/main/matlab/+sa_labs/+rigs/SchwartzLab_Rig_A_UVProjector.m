4e0a17c1d54524dc334f1c4bfdb6e0d4581ea3b9
Sam Cooler <scooler@gmail.com>
2019-05-14 11:06:38 -0500

    updated light calibration for NDF8 color

--- src/main/matlab/+sa_labs/+rigs/SchwartzLab_Rig_A_UVProjector.m
+++ src/main/matlab/+sa_labs/+rigs/SchwartzLab_Rig_A_UVProjector.m
@@ -6,19 +6,22 @@ classdef SchwartzLab_Rig_A_UVProjector < sa_labs.rigs.SchwartzLab_Rig_Base
         testMode = false;
         filterWheelNdfValues = [2, 4, 5, 6, 7, 8];
         filterWheelDefaultValue = 5;
-        filterWheelAttenuationValues_Green = [1, .01, .001, .0001, 0, 0];
-        filterWheelAttenuationValues_Blue = [1, .0073, .00072, .0001, .00001, .000001];
-        filterWheelAttenuationValues_UV = [1, .0049, .0027, 0, 0, 0];
+        
+        % updated 5/14/19 SAC
+        filterWheelAttenuationValues_Green = [1, .0106, .0016, .0001, 0, .0663];
+        filterWheelAttenuationValues_Blue = [1, .0113, .0027, .0001, .00001, .0578];
+        filterWheelAttenuationValues_UV = [1, .0026, 0, 0, 0, .948];
+        
+        fitBlue = [-4.12969088427779e-12,1.76186023670153e-09,-7.25142913686745e-09];
+        fitGreen =[-4.89191631033382e-12,2.25392165538544e-09,-1.31352770835765e-08];
+        fitUV = [-2.78028975564508e-14,2.23045870111502e-11,1.89444080767412e-09];
+        
         micronsPerPixel = 1.38;
         frameTrackerPosition = [0,0];
         frameTrackerSize = [550,550];
         filterWheelComPort = 'COM8';
         orientation = [false, true];
         angleOffset = 0;
-        
-        fitBlue = [3.46902552495379e-14,-1.37685313164116e-11,3.15370683929025e-09,-1.44962712464649e-08];
-        fitGreen =[2.58038688962281e-14,-1.29545015871977e-11,3.60389025129295e-09,-2.16775315211613e-08];
-        fitUV = [2.96965590127296e-16,-1.32952775434061e-13,4.14642042203279e-11,2.82460395102315e-09];
         
         projectorColorMode = 'uv2'; % Rig A has MkII projector
         numberOfAmplifiers = 2;
