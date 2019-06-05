aeebb2feff1002afe37030f00a34f86384c08646
samcooler <scooler@gmail.com>
2019-06-05 16:58:53 -0500

    moving object now functioning

--- src/main/matlab/+sa_labs/+protocols/+stage/MovingObject.m
+++ src/main/matlab/+sa_labs/+protocols/+stage/MovingObject.m
@@ -1,24 +1,25 @@
 classdef MovingObject < sa_labs.protocols.StageProtocol
-
+    
     properties
-        preTime = 0                     % Bar leading duration (ms)
-        tailTime = 0                    % Bar trailing duration (ms)
+        preTime = 50                     % Object leading duration (ms)
+        tailTime = 50                    % Object trailing duration (ms)
         stimTime = 800
-        intensity = 1.0                 % Bar light intensity (0-1)
+        intensity = 1.0                 % Object light intensity (0-1)
         setDirections = 8               % num directions in circle, or standard range
-        setSpeeds = 1000                % 
-        setDiameters = 2500             % Bar distance (um)
-        setShapes = 'circle'            
+        setSpeeds = [1000,1000,1]                %
+        setDiameters = [100,100,1]              % Object diameter (um)
+        setOffsets = [0,0,0]
+        setShapes = 'circle'
         centerTimeShift = -200;         % bias stimulus to before crossing the center (ms)
         numberOfCycles = 3              % Number of times through the set
     end
     
     properties (Hidden)
         version = 1                     % v1: initial version
-        parameters                      % matrix of all epoch params, [angle, offset, side]
+        parameters = []                      % matrix of all epoch params, [angle, offset, side]
         currentParameters
-        setShapesType = symphonyui.core.PropertyType('char', 'row', {'both', 'left', 'right'})
-       
+        setShapesType = symphonyui.core.PropertyType('char', 'row', {'circle', 'rectangle'})
+        
         responsePlotMode = 'polar';
         responsePlotSplitParameter = 'direction';
     end
@@ -36,71 +37,41 @@ classdef MovingObject < sa_labs.protocols.StageProtocol
     
     methods
         
-        function out = parseParamInput(inpt, logScale, twoSided)
-            if nargin < 2
-                logScale = 0;
-            end
-            if nargin < 3
-                twoSided = 0;
-            end
-            
-            l = length(inpt);
-            switch l
-                case 0
-                    out = 0;
-                case 1
-                    out = inpt(1);
-                case 2
-                    if logScale
-                        out = logspace(log10(inpt(1)), log10(inpt(2)), 8);
-                    else
-                        out = linspace(inpt(1), inpt(2), 8);
-                    end
-                case 3
-                    if logScale
-                        out = logspace(log10(inpt(1)), log10(inpt(2)), inpt(3));
-                    else
-                        out = linspace(inpt(1), inpt(2), inpt(3));
-                    end
-            end
-            
-            if twoSided % mostly for offset
-                out = horzcat(fliplr(out), out(2:end)); % don't repeat middle value
-            end
-        end
-            
-               
+        
+        
+        
         function prepareRun(obj)
-
-            
-            obj.numberOfOffsets = length(obj.offsets);
-            
-            
+            obj.generateParameters();
+            fprintf('Generated %g parameter sets\n', size(obj.parameters, 1))
             prepareRun@sa_labs.protocols.StageProtocol(obj);
+        end
+        
+        function generateParameters(obj)
+%             obj.directions = obj.directions(randperm(length(obj.directions)));
+%             obj.speeds = obj.speeds(randperm(length(obj.speeds)));
+%             obj.offsets = obj.offsets(randperm(length(obj.offsets)));
+%             obj.diameters = obj.diameters(randperm(length(obj.diameters)));
+            
+            params = [];
+            
+            for di = randperm(length(obj.directions))
+                for si = randperm(length(obj.speeds))
+                    for oi = randperm(length(obj.offsets))
+                        for diami = randperm(length(obj.diameters))
+                            params = vertcat(params, [obj.directions(di), obj.speeds(si), obj.offsets(oi), obj.diameters(diami)]);
+                        end
+                    end
+                end
+            end
+            obj.parameters = params(randperm(size(params, 1)), :);
+            
         end
         
         function prepareEpoch(obj, epoch)
             
             index = mod(obj.numEpochsPrepared, size(obj.parameters, 1));
             if index == 0
-                obj.directions = obj.angles(randperm(length(obj.directions)));
-                obj.speeds = obj.speeds(randperm(length(obj.speeds)));
-                obj.offsets = obj.offsets(randperm(length(obj.offsets)));
-                obj.diameters = obj.diameters(randperm(length(obj.diameters)));
-                
-                params = [];
-                
-                for di = 1:length(obj.directions)
-                    for si = 1:length(obj.speeds)
-                        for oi = 1:length(obj.offsets)
-                            for diami = 1:length(obj.diameters)
-                                params = vertcat(params, [obj.directions(ai), obj.speeds(si), obj.offsets(oi), obj.diameters(diami)]);
-                            end
-                        end
-                    end
-                end
-                obj.parameters = params(randperm(size(params, 1)), :);
-
+                obj.generateParameters();
             end
             
             obj.currentParameters = obj.parameters(index + 1, :);
@@ -113,7 +84,7 @@ classdef MovingObject < sa_labs.protocols.StageProtocol
             fprintf('Next epoch: direction %g, speed %g, offset %g, diameter %g\n', obj.currentParameters(1), obj.currentParameters(2), obj.currentParameters(3), obj.currentParameters(4))
             
             prepareEpoch@sa_labs.protocols.StageProtocol(obj, epoch);
-        end        
+        end
         
         function p = createPresentation(obj)
             currentDirection = obj.currentParameters(1);
@@ -144,7 +115,7 @@ classdef MovingObject < sa_labs.protocols.StageProtocol
             yStep = pixelSpeed * sind(currentDirection);
             
             cts = obj.centerTimeShift;
-
+            
             xStartPos = canvasSize(1)/2 - (pixelDistance / 2) * cosd(currentDirection) + pixelOffset * cosd(currentDirection - 90);
             yStartPos = canvasSize(2)/2 - (pixelDistance / 2) * sind(currentDirection) + pixelOffset * sind(currentDirection - 90);
             
@@ -165,42 +136,76 @@ classdef MovingObject < sa_labs.protocols.StageProtocol
             obj.setColorController(p, object);
             
         end
-                
+        
         
         function totalNumEpochs = get.totalNumEpochs(obj)
-            totalNumEpochs = obj.numberOfCycles * size(obj.parameters, 1);
-        end        
-
-%         function stimTime = get.stimTime(obj)
-%             % to be written
-%             stimTime = 1000;
-%         end
-        
-        function directions = get.directions(obj)
-            if length(obj.setDirections) == 1
-                numberOfAngles = obj.setDirections(1);
-                directions = round(0:360/numberOfAngles:(360-.01));
-            else
-                directions = obj.parseParamInput(obj.setDirections);
-%                 numberOfAngles = obj.setDirections(3);
-%                     d = obj.setDirections(2) - obj.setDirections(1);
-%                     directions = round(obj.setDirections(1):d/numberOfAngles:((obj.setDirections(2)-.01)));
+                totalNumEpochs = obj.numberOfCycles * size(obj.parameters, 1);
+            end
+            
+            %         function stimTime = get.stimTime(obj)
+            %             % to be written
+            %             stimTime = 1000;
+            %         end
+            
+            
+            function out = parseParamInput(~, inpt, logScale, twoSided)
+                if nargin < 2
+                    logScale = 0;
+                end
+                if nargin < 3
+                    twoSided = 0;
+                end
+                
+                l = length(inpt);
+                switch l
+                    case 0
+                        out = 0;
+                    case 1
+                        out = inpt(1);
+                    case 2
+                        if logScale
+                            out = logspace(log10(inpt(1)), log10(inpt(2)), 8);
+                        else
+                            out = linspace(inpt(1), inpt(2), 8);
+                        end
+                    case 3
+                        if logScale
+                            out = logspace(log10(inpt(1)), log10(inpt(2)), inpt(3));
+                        else
+                            out = linspace(inpt(1), inpt(2), inpt(3));
+                        end
+                end
+                
+                if twoSided % mostly for offset
+                    out = horzcat(-1*fliplr(out), 0, out); % don't repeat middle value
+                end
+            end
+            
+            function directions = get.directions(obj)
+                if length(obj.setDirections) == 1
+                    numberOfAngles = obj.setDirections(1);
+                    directions = round(0:360/numberOfAngles:(360-.01));
+                else
+                    directions = obj.parseParamInput(obj.setDirections);
+                    %                 numberOfAngles = obj.setDirections(3);
+                    %                     d = obj.setDirections(2) - obj.setDirections(1);
+                    %                     directions = round(obj.setDirections(1):d/numberOfAngles:((obj.setDirections(2)-.01)));
+                end
+            end
+            
+            
+            function speeds = get.speeds(obj)
+                speeds = obj.parseParamInput(obj.setSpeeds, 1, 0);
+            end
+            
+            function offsets = get.offsets(obj)
+                offsets = obj.parseParamInput(obj.setOffsets, 1, 1);
+            end
+            
+            function diameters = get.diameters(obj)
+                diameters = obj.parseParamInput(obj.setDiameters, 1, 0);
             end
         end
-
         
-        function speeds = get.speeds(obj)
-            speeds = obj.parseParamInput(obj.setSpeeds, 1, 0);
-        end
-        
-        function offsets = get.offsets(obj)
-            offsets = obj.parseParamInput(obj.setOffsets, 1, 1);
-        end
-        
-        function diameters = get.diameters(obj)
-            diameters = obj.parseParamInput(obj.setDiameters, 1, 0);
-        end
     end
     
-end
-
