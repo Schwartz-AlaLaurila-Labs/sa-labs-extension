af453f2564065395a7bcf1d3c276970551a0da1a
zfj1 <39531155+zfj1@users.noreply.github.com>
2021-05-10 13:41:34 -0500

    fix filter wheel error

--- src/main/matlab/+sa_labs/+rigs/SchwartzLab_Rig_B_testing.m
+++ src/main/matlab/+sa_labs/+rigs/SchwartzLab_Rig_B_testing.m
@@ -5,11 +5,12 @@ classdef SchwartzLab_Rig_B_testing < sa_labs.rigs.SchwartzLab_Rig_B
             self@sa_labs.rigs.SchwartzLab_Rig_B(true); %delay init
             self.host = 'localhost';
             self.lcr = @MockLightCrafter4500;
+            self.filterWheelComPort = -1;
 
             if nargin < 1
                 delayInit = false;
             end
-            
+
             if ~delayInit
                 self.initializeRig();
             end
