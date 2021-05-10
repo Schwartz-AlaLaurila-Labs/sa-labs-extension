07f57086ed6eba103773df0795951b71e750f2cd
zfj1 <39531155+zfj1@users.noreply.github.com>
2021-05-10 12:50:55 -0500

    missing scope

--- src/main/matlab/+sa_labs/+rigs/SchwartzLab_Rig_B_testing.m
+++ src/main/matlab/+sa_labs/+rigs/SchwartzLab_Rig_B_testing.m
@@ -1,4 +1,4 @@
-classdef SchwartzLab_Rig_B_testing < SchwartzLab_Rig_B
+classdef SchwartzLab_Rig_B_testing < sa_labs.rigs.SchwartzLab_Rig_B
     properties
         %overwrite the default rig properties so that we don't need to be
         %connected to anything
