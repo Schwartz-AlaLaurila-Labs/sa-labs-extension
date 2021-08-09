5916e7271b94e77517cf42b4f6ebe69ae28bd855
davidswygart <davidswygart2015@u.northwestern.edu>
2021-08-09 14:31:54 -0500

    Update photoIsom2_triColor.m
    Fixed error in pulling the wrong data from the rig config to calculate the blue_Mcone spectralOverlap

--- src/main/matlab/+sa_labs/+util/photoIsom2_triColor.m
+++ src/main/matlab/+sa_labs/+util/photoIsom2_triColor.m
@@ -1,62 +1,35 @@
 function [Rstar, Mstar, Sstar ] = photoIsom2_triColor( bluIntenVal, grnIntenVal, uvIntenVal, color, fitBlue, fitGreen, fitUV, NDF_attenuation_Blue, NDF_attenuation_Green, NDF_attenuation_UV, spectralOverlap_Blue, spectralOverlap_Green, spectralOverlap_UV)
 %R*, M*, S* for given intensity parameters
 
-
-% Written by Adam Mani on 3/14
-% adapted for Upper Projector TriColor Protocols by TA 3/16
-% updated to symphony 2 by Sam 2/17
-
-%intensity vs LED current fit coefficients (2nd deg polynmial)
-%measured RigA Adam 3/28/14
-%d=400um, units=nW
-
-%4/14/14 photoIsom2, Adam:
-%pass fitBlue, fitGreen as parameters, uW PER SQUARE MICRON.
-%ANY degree polynomial
-
-%fitBlue and fitGreen =1x(degree of poly+1) double.
-%To get coefficients:
-%1) Make a table of power vs LED intensity
-%2) Divide power by spot area in square microns.
-%3) Use cftool to fit a 2nd deg. polynomial and save its coefficients.
-
-
-
-%scaling facotrs
-
-uW_to_Watt = 10^(-6);
+%fitBlue, fitGreen, FitUV: polonomial fit of LED values to W PER SQUARE MICRON.
+%Use projectorIntensityCoeff.m to calculate fits.
 
 %photoreceptor area (MICRON^2)
-
 pr_area = 0.37;
 pr_area_rod = 0.5;
 
 %INTENSITY IN WATTS PER ROD or CONE:
 x = bluIntenVal;
 y = polyval(fitBlue,x) * NDF_attenuation_Blue;
-blueInten_rod = y.*uW_to_Watt.*pr_area_rod;
-blueInten_cone = y.*uW_to_Watt.*pr_area;
+blueInten_rod = y.*pr_area_rod;
+blueInten_cone = y.*pr_area;
 
 x = grnIntenVal;
 y = polyval(fitGreen,x) * NDF_attenuation_Green;
-greenInten_rod = y.*uW_to_Watt.*pr_area_rod;
-greenInten_cone = y.*uW_to_Watt.*pr_area;
+greenInten_rod = y.*pr_area_rod;
+greenInten_cone = y.*pr_area;
 
 x = uvIntenVal;
 y = polyval(fitUV,x) * NDF_attenuation_UV;
-uvInten_rod = y.*uW_to_Watt.*pr_area_rod;
-uvInten_cone = y.*uW_to_Watt.*pr_area;
+uvInten_rod = y.*pr_area_rod;
+uvInten_cone = y.*pr_area;
 
 %Spectra pre-factors for LED-photoreceptor combinations
 %calculated using "calbrationPrefactorCalc_Adam"
 
-
-% Values from Rig A UV projector, Sam 12/2018
-
-
 blue_Rod = spectralOverlap_Blue(1);
-blue_Scone = spectralOverlap_Blue(1);
-blue_Mcone = spectralOverlap_Blue(1);
+blue_Scone = spectralOverlap_Blue(2);
+blue_Mcone = spectralOverlap_Blue(3);
 
 green_Rod = spectralOverlap_Green(1);
 green_Scone = spectralOverlap_Green(2);
