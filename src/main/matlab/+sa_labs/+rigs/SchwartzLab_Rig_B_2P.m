3f32e8db9c7013e5eaa682b65df6c6ef8a469bf2
davidswygart <davidswygart2015@u.northwestern.edu>
2021-08-10 13:00:58 -0500

    Create SchwartzLab_Rig_B_2P.m
    Added a new Rig Config for use during functional imaging (simultaneous 2P imaging and projector stimulation).  These calibration values assume you have swiveled the Bluepass filter in place near the NDF wheel.

---
+++ src/main/matlab/+sa_labs/+rigs/SchwartzLab_Rig_B_2P.m
@@ -0,0 +1,71 @@
+classdef SchwartzLab_Rig_B_2P < sa_labs.rigs.SchwartzLab_Rig_Base
+    
+    properties
+        % properties not accessible here; have to be fed into a device to work
+        % This rigConfiguration is for use when the blue passband filter (ET460) is
+        % swiveled into place to allow for simultaneous 2P imaging and projector stimulation.
+        % Only blue projector light should be used.
+        rigName = 'Schwartz Lab Rig B 2P Imaging';
+        testMode=false;
+        filterWheelNdfValues = [1, 2, 3, 4, 5, 0]; %updated 4/2/21 - David
+        filterWheelDefaultValue = 5;
+        
+        filterWheelAttenuationValues_Blue = [0.087209302	0.00744186	0.000612403	6.77132E-05	6.47287E-06 1];%updated 7/1/21 - David
+        filterWheelAttenuationValues_Green = 0;%Only use blue LED when bluepass filter is in place. - David
+        filterWheelAttenuationValues_UV = 0; %Only use blue LED when bluepass filter is in place. - David
+        
+        fitBlue = [8.05816831848421e-18,-5.14680445171829e-15,1.73086178980379e-12,-4.00660672245227e-11];%updated 7/1/21 - David
+        fitGreen = 0;%Only use blue LED when bluepass filter is in place. - David
+        fitUV = 0;%Only use blue LED when bluepass filter is in place. - David
+        
+        micronsPerPixel = 1.65 %updated 2/25/21 -David -- There is a slight discrepancy between X and Y axis.  1.7 is best for X, 1.6 if best for Y.  I split the difference.
+
+        frameTrackerPosition = [0,1280];%updated 02/23/21 -David
+
+        frameTrackerSize = [100,100];%updated 02/23/21 -David
+       
+        filterWheelComPort = 'COM5';
+        orientation = [false, true]; %[flip Y, flip X]
+        angleOffset = 0; %Does not actually change presentation.  Is saved in epoch data so it could be used in analysis, but it isn't used now.
+        
+        %Overlap of the Rod, S_cone, and M_cone spectrum with each LED. Must be in order [1 Rod, 2 S cone, 3 M cone]
+        spectralOverlap_Blue = [4.86e+18,5.57e+15,3.87e+18];%updated 7/1/21 - David
+        spectralOverlap_Green = [2.47e+18,2.65e+17,2.36e+18];%updated 7/1/21 - David
+        spectralOverlap_UV = [1.35e+18,3.02e+17,1.41e+18];%updated 7/1/21 - David
+        
+        projectorColorMode = 'uv';
+        numberOfAmplifiers = 2;
+        
+        host = '192.168.0.3'; %What is the ip address to connect to the stage computer?  If Stage is running on this computer, use 'localhost'.
+        daq_type = 'NI'; %What brand data aquisition board is being used?  'Heka' or 'NI'
+    end
+    
+    methods
+        
+        function obj = SchwartzLab_Rig_B(delayInit)
+            %{Port, bit number, unit} for any datastreams. 
+            % unit = 0 for unitless.  
+            % bit number = -1 for analog.
+            % Comment out if you don't want to use.
+     
+            obj.daqStreams('Oscilloscope Trigger') = {'doport0', 0, 0}; %
+            obj.daqStreams('Stim Time Recorder') = {'doport0', 1, 0}; %
+            %daqStreams('Scanhead Trigger') = {'doport1', 2, 0}; %
+            %daqStreams('Optogenetics Trigger') = {'doport1', 3, 0}; %
+            %daqStreams('Scanhead Trigger') = {'doport1', 2, 0}; %
+            %daqStreams('Excitatory conductance') = {'ao2', -1, 'V'}; %
+            %daqStreams('Inhibitory conductance') = {'ao3', -1, 'V'}; %
+            
+            if nargin < 1
+                delayInit = false;
+            end
+            
+            if ~delayInit
+                obj.initializeRig();
+            end
+        end
+
+    end
+    
+end
+
