06355c8e75508bc8fa41bf5f1aacb6a13ddc3584
Zach <z.jessen@gmail.com>
2022-02-04 15:09:03 -0600

    updates to euler stimulus

--- src/main/matlab/+sa_labs/+protocols/+stage/ColorOpponentCheckerboard.m
+++ src/main/matlab/+sa_labs/+protocols/+stage/ColorOpponentCheckerboard.m
@@ -1,21 +1,28 @@
 classdef ColorOpponentCheckerboard < sa_labs.protocols.StageProtocol
-       
+    %TODO:
+    %   - set up with multiple LEDs
+    %   - texture size as a parameter?
+    %   - 
+    
     properties
         %times in ms
-        preTime = 500	% Spot leading duration (ms)
-        stimTime = 1000	% Spot duration (ms)
-        tailTime = 1000	% Spot trailing duration (ms)
+        preTime = 500	% Texture leading duration (ms)
+        stimTime = 1000	% Texture duration (ms)
+        tailTime = 1000	% Texture trailing duration (ms)
         
         intensity = 0.5
         
         textureSize = 200; % um
         numberOfEpochs = 200
+
+        alternatePatterns = false
     end
     
     properties (Hidden)
         version = 1;
-       responsePlotMode = 'cartesian'; 
-        responsePlotSplitParameter = 'currentTextureSize';
+        currentTexturePattern
+        responsePlotMode = 'cartesian'; 
+        responsePlotSplitParameter = 'currentTexturePattern';
     end
     
     properties (Dependent, Hidden)
@@ -25,8 +32,23 @@ classdef ColorOpponentCheckerboard < sa_labs.protocols.StageProtocol
     methods
         
         function prepareEpoch(obj, epoch)
-            %passing for now
-            epoch.addParameter('currentTextureSize',obj.textureSize); %temporary
+            %TODO: remove this?
+            obj.currentTexturePattern = obj.primaryObjectPattern;
+            if obj.numberOfPatterns > 1
+                if obj.alternatePatterns
+                    if mod(obj.numEpochsPrepared, 2) == 1
+                        obj.currentTexturePattern = obj.secondaryObjectPattern;
+                        currentTextureColor = obj.colorPattern2;
+                    else
+                        obj.currentTexturePattern = obj.primaryObjectPattern;
+                        currentTextureColor = obj.colorPattern1;
+                    end
+                    disp(currentTextureColor)
+                end
+            end
+            epoch.addParameter('currentTexturePattern', obj.currentTexturePattern);
+           
+            % Call the base method.
             prepareEpoch@sa_labs.protocols.StageProtocol(obj, epoch);
             
         end
@@ -36,6 +58,8 @@ classdef ColorOpponentCheckerboard < sa_labs.protocols.StageProtocol
             
             % Image is M-by-N (grayscale), M-by-N-by-3 (truecolor)
             texture = stage.builtin.stimuli.Image(randi(255,[200,200],'uint8'));
+            texture.color = obj.intensity;
+            texture.opacity = 1;
             % texture = stage.builtin.stimuli.Ellipse();
             % texture.radiusX = obj.textureSize;
             % texture.radiusY = obj.textureSize;
@@ -43,7 +67,7 @@ classdef ColorOpponentCheckerboard < sa_labs.protocols.StageProtocol
             
             canvasSize = obj.rig.getDevice('Stage').getCanvasSize();
             texture.position = canvasSize / 2;
-            texture.size = [obj.textureSize obj.textureSize];
+            texture.size = [obj.textureSize obj.textureSize]; %TODO: pixels2microns step
             
             p.addStimulus(texture);
             
---
+++ src/main/matlab/+sa_labs/+protocols/+stage/MaximallyDiscriminatingStimulus.m
@@ -0,0 +1,15 @@
+classdef MaximallyDiscriminatingStimulus < sa_labs.protocols.StageProtocol
+    % TODO:
+    %   - colors
+    %       - check our LED spectra vs. theirs (silent substitution?)
+    %       - consider luminance level
+    %   - scaling
+    %  - stage.builtin.stimuli.Movie(filename)
+
+    properties
+    end
+    
+    methods
+    end
+end
+
