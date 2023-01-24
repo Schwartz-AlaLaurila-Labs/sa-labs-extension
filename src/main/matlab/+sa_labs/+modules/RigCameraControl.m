39ae268a3e1dbb4a89f7ce06093467af4cb28167
David <davidswygart@gmail.com>
2023-01-24 12:54:43 -0600

    New

--- src/main/matlab/+sa_labs/+modules/RigCameraControl.m
+++ src/main/matlab/+sa_labs/+modules/RigCameraControl.m
@@ -7,6 +7,19 @@ classdef RigCameraControl < symphonyui.ui.Module
         overlap = .1
         width = 720
         height = 480
+        correction = false
+    end
+    
+    properties(Access = private)
+        img = []
+        frms = {};
+        tforms = {};
+        
+        lastx = [];
+        lasty = [];
+        lastpos = [];
+        lastmag = ''; 
+        
     end
 
     properties (Dependent)
@@ -16,6 +29,7 @@ classdef RigCameraControl < symphonyui.ui.Module
     properties (Hidden)
         mags = {'10X','60X'}
         mag_ind = 1
+        
     end
 
     methods
@@ -51,6 +65,13 @@ classdef RigCameraControl < symphonyui.ui.Module
             
             uicontrol(...
                 'parent',menu,...
+                'style','togglebutton',...
+                'string','Illumination filter',...
+                'value', self.correction,...
+                'Callback', @self.onSetCorrection);
+            
+            uicontrol(...
+                'parent',menu,...
                 'style','text',...
                 'string','Relative overlap: ');            
             
@@ -66,17 +87,29 @@ classdef RigCameraControl < symphonyui.ui.Module
                 'string',self.mags,...
                 'value',self.mag_ind,...
                 'Callback',@self.onSetMag);   
+            
+            buttons = uix.VBox(...
+                'parent', menu,...
+                'padding', 1);
 
             uicontrol(...
-                'parent',menu,...
+                'parent',buttons,...
                 'style','pushbutton',...
                 'string','Start',...
                 'Callback',@self.start);
-            
+
+            uicontrol(...
+                'parent',buttons,...
+                'style','pushbutton',...
+                'string','Save',...
+                'Callback',@self.save);
+
             self.display = axes(...
                 'parent',layout);
 
             set(layout, 'Heights', [50, -1]);
+            
+            
         end
 
         function self = onSetNRows(self, e, ~)
@@ -85,6 +118,10 @@ classdef RigCameraControl < symphonyui.ui.Module
         
         function self = onSetNCols(self, e, ~)
             self.ncols = str2double(e.String);
+        end
+        
+        function self = onSetCorrection(self, e, ~)
+            self.correction = e.Value;
         end
 
         function self = onSetOverlap(self, e, ~) 
@@ -96,28 +133,197 @@ classdef RigCameraControl < symphonyui.ui.Module
         end        
 
         function mag = get.mag(self)
-            mag = self.mags(self.mag_ind);
+            mag = self.mags{self.mag_ind};
         end
 
-        function start(self, ~, ~)      
-            camera = self.configurationService.getDevice('RigCamera');
+        function um = pix2um(self, pix)
+            %% TODO: these need to be calibrated appropriately
+            % ideally as part of the rig config
             
-            scale = (1-self.overlap) .* self.pix2um(self.mag) .* [self.width, self.height];
+            switch self.lastmag
+            case '10X'
+                um = 1.3783 * pix;
+%                 um = 1.5 * pix;
+            case '60X'
+                um = 0.2417 * pix;
+%                 um = 0.65 * pix;
+            otherwise
+                error('Unknown magnification');
+            end
+        end
+        
+        %%
+        function start(self, ~, ~)      
+            %% clear the display
+            cla(self.display);
+            set(self.display,'color','none');
+            drawnow;
+
+            %% get handles to the devices
+            camera = self.configurationService.getDevice('RigCamera');
+            stage = Stage.Stage();
+            self.lastpos = stage.pos;
+
+
+            %% configure the image settings
+            self.lastmag = self.mag;
+            scale = (1-self.overlap) .* self.pix2um([self.width, self.height]);
             [x,y] = meshgrid( - (self.ncols/2 - .5) :  (self.ncols/2 - .5), - (self.nrows/2 - .5) :  (self.nrows/2 - .5));
             
-            x = x .* scale(1);
-            y = y .* scale(2);
+            self.lastx = round(- x .* scale(1))';
+            self.lasty = round(y .* scale(2))';
+            x = [self.lastx(:); 0];
+            y = [self.lasty(:); 0];
+            
+            stage.pos = self.lastpos + [x(1), y(1), 0]'; %initiate the first movement, while we continue setting up
 
-            for n = 1:numel(x)
-                %% move to (x,y)
-                %% take image
-                img = camera.getFrame();
-                %%   stitch?
-                %%   display image
-                imshow(self.display, img);
+            %% configure the stitching settings
+            glob = imref2d([... %the global view onto which images will be warped
+                round((self.width * self.ncols) - (self.overlap* self.width * (self.ncols-1))), ...
+                round((self.height * self.nrows) - (self.overlap* self.height * (self.nrows-1)))]);
+            [opt, met] = imregconfig('monomodal');
+            grads = cell(self.nrows, self.ncols);
+            self.tforms = cell(self.nrows, self.ncols);
+            self.frms = cell(self.nrows, self.ncols);
+            
+            imgs = nan(...
+                round((self.width * self.ncols) - (self.overlap* self.width * (self.ncols-1))), ...
+                round((self.height * self.nrows) - (self.overlap* self.height * (self.nrows-1))), ...
+                self.nrows * self.ncols, 'single');
+
+            dr = self.height * (1 - self.overlap);% + 50;
+            dc = self.width * (1 - self.overlap);% + 50; 
+            pause(0.05);
+            for r = 1:self.nrows
+                for c = 1:self.ncols
+                    %% move to (x,y)
+                    while stage.status % make sure we've finished moving
+%                         pause(1-self.overlap); %helps with motion blur
+                    end
+                    pause(1-self.overlap);
+                    
+                    %% take image   
+                    frm = camera.getFrame();
+                    self.frms{r,c} = single(squeeze(frm(1,:,:)));
+                    for n = 1:4
+                        frm = camera.getFrame();
+                        self.frms{r,c} = self.frms{r,c} + single(squeeze(frm(1,:,:)));
+                    end
+                    self.frms{r,c} = self.frms{r,c} / 5;
+                    
+                    %% initiate move to next location
+                    stage.pos = self.lastpos + [x(c + self.ncols*(r-1) + 1), y(c + self.ncols*(r-1) + 1), 0]';
+                    
+                    %% prepare image for stitching image
+                    grads{r,c} = imgradient(self.frms{r,c});
+                    %NOTE: we use the gradient to filter out lowpass artifacts
+                    % these are mostly due to uneven illumination of the imaging field
+
+                    %% align image
+                    if r == 1 && c == 1
+                        self.tforms{r,c} = affine2d([1,0,0;0,1,0;0,0,1]);                        
+                    else                        
+                        if c == 1
+                            g = grads{r-1,c};
+                            t = self.tforms{r-1,c};
+                            a = affine2d([1,0,0;0,1,0;dr,0,1]);
+                        else
+                            g = grads{r,c-1};
+                            t = self.tforms{r,c-1};
+                            a = affine2d([1,0,0;0,1,0;0,dc,1]);
+                        end
+                        tform = imregtform(...
+                                grads{r,c}(50:end-50,50:end-50),g(50:end-50,50:end-50),... 
+                                'translation', opt, met,...
+                                'initialtransformation',a...
+                                );
+                        self.tforms{r,c} = affine2d(tform.T * t.T); %compose the transforms
+                    end
+                    imgs(:,:,c + self.ncols*(r-1)) = imwarp(self.frms{r,c}, self.tforms{r,c}, 'outputview', glob, 'fillvalues', nan);
+                    
+                    %% stitch
+                    self.img = nanmean(imgs, 3)';
+
+                    %% display
+                    imagesc(self.display,self.img);
+                    axis(self.display, 'xy')
+                    colormap(self.display, gray(256));
+                    drawnow;
+                end
+            end
+            
+            %% remove illumination artifact
+            if self.correction
+                frms_ = cat(3,self.frms{:});
+                artifact = imgaussfilt(median(frms_,3),5);
+                for i=1:size(imgs,3)
+                    imgs(:,:,i) = imwarp(self.frms{i}-artifact, self.tforms{i}, 'outputview',glob,'fillvalues',nan);
+                end
+                self.img = nanmean(imgs, 3)';
+                self.img = self.img - min(self.img,[],'all') + min(frms_,[],'all');
+
+                imagesc(self.display,self.img);
+                axis(self.display, 'xy')
+                colormap(self.display, gray(256));
                 drawnow;
             end
+            %% clean up
+            delete(stage);
 
+        end
+        
+        function save(self, ~, ~)
+            %mkdir
+            [fname, path] = uiputfile('*.tif','Select location for stitched image',sprintf('%s_stitched.tif',datestr(datetime('today'), 'mmddyyB')));
+            [~,fname_noext,~] = fileparts(fname);
+            
+            tif = Tiff(sprintf('%s%s%s',path,filesep,fname), 'w');
+            tif.setTag('ImageLength', size(self.img,1));
+            tif.setTag('ImageWidth', size(self.img,2));
+            tif.setTag('Photometric', Tiff.Photometric.MinIsBlack);
+            tif.setTag('BitsPerSample', 8);
+            tif.setTag('SamplesPerPixel', 1);
+            tif.setTag('PlanarConfiguration', Tiff.PlanarConfiguration.Chunky);
+            tif.setTag('RowsPerStrip', 16);
+            tif.setTag('ResolutionUnit',Tiff.ResolutionUnit.Centimeter);
+            tif.setTag('XResolution', 10000/self.pix2um(1));
+            tif.setTag('YResolution', 10000/self.pix2um(1));
+            
+            tif.write(flipud(uint8(self.img)));
+            
+            tif.close();
+            if numel(self.frms) == 1
+                return
+            end
+            for i=1:numel(self.frms)
+                
+                
+                comment = sprintf(...
+                    'Stage coordinates (um): %d,%d,%d. Relative coordinates (um): %d,%d. Estimated translation (um): %f,%f.',...
+                    self.lastpos(1) + self.lastx(i), self.lastpos(2) + self.lasty(i), self.lastpos(3),...
+                    self.lastx(i), self.lasty(i),...
+                    self.pix2um(self.tforms{i}.T(3)), self.pix2um(self.tforms{i}.T(6))... 
+                    );
+                tif = Tiff(sprintf('%s%s%s_%03d.tif',path,filesep,fname_noext,i), 'w');
+                
+                tif.setTag('ImageLength', size(self.frms{i},2));
+                tif.setTag('ImageWidth', size(self.frms{i},1));
+                tif.setTag('Photometric', Tiff.Photometric.MinIsBlack);
+                tif.setTag('BitsPerSample', 8);
+                tif.setTag('SamplesPerPixel', 1);
+                tif.setTag('PlanarConfiguration', Tiff.PlanarConfiguration.Chunky);
+                tif.setTag('RowsPerStrip', 16);
+                tif.setTag('ResolutionUnit',Tiff.ResolutionUnit.Centimeter);
+                tif.setTag('XResolution', 10000/self.pix2um(1));
+                tif.setTag('YResolution', 10000/self.pix2um(1));
+                tif.setTag('ImageDescription', comment);
+                
+                tif.write(flipud(uint8(self.frms{i})'));
+                
+                tif.close();
+            end
+            
+            
         end
     end
 
---
+++ src/main/matlab/+sa_labs/+rigs/SchwartzLab_Rig_B_2P_1amp.m
@@ -0,0 +1,86 @@
+classdef SchwartzLab_Rig_B_2P_1amp < sa_labs.rigs.SchwartzLab_Rig_Base
+    
+    properties
+        % properties not accessible here; have to be fed into a device to work
+        % This rigConfiguration is for use when the blue passband filter (ET460) is
+        % swiveled into place to allow for simultaneous 2P imaging and projector stimulation.
+        % Only blue projector light should be used.
+        rigName = 'Schwartz Lab Rig B 2P 1 amp';
+        testMode=false;
+        filterWheelNdfValues = [1, 2, 3, 4, 5, 0]; %updated 4/2/21 - David
+        filterWheelDefaultValue = 5;
+        
+        filterWheelAttenuationValues_Blue = [0,0,0,0,0,0];%updated 8/8/22 - Zach
+        filterWheelAttenuationValues_Green = [0,0,0,0,0,0];%Only use uv LED when uvpass filter is in place. - Zach
+        filterWheelAttenuationValues_UV = [0.05434782609 0.003333333333 0.0001893719807 0.00001572463768 0.000001132850242 1]; %updated 8/8/22 - Zach
+        
+        fitBlue = 0;%updated 8/8/22 - Zach
+        
+        fitGreen = 0;%updated 8/8/22 - Zach
+        fitUV = 1.0e-11 *[-0.000000122471269  -0.000012818464172   0.097151912916748  -0.118471538418705];%updated 8/8/22 - Zach
+
+        micronsPerPixel = 1.3 %updated 7/18/22 Zach
+
+        % frameTrackerPosition = [160, 1280]; %updated 7/25/22 Zach
+        % frameTrackerDuration = 0.1; %updated 5/26/22 Zach
+        % frameTrackerBackgroundSize = [360, 2560]; %updated 7/25/22 Zach
+        % frameTrackerSize = [240, 640]; %updated 7/25/22 Zach
+        frameTrackerPosition = [0, 300]; %updated 9/1/22 David
+        frameTrackerDuration = 0.05; %updated 9/1/22 David
+        frameTrackerBackgroundSize = [800, 2000]; %updated 9/1/22 David
+        frameTrackerSize = [80, 400]; %updated 9/1/22 David
+        canvasTranslation = [117, 0]; %updated 6/9/22 Zach
+       
+        filterWheelComPort = 'COM5';
+        orientation = [false, true]; %[flip Y, flip X]
+        angleOffset = 180; %Does not actually change presentation.  Is saved in epoch data so it could be used in analysis, but it isn't used now.
+        
+        %Overlap of the Rod, S_cone, and M_cone spectrum with each LED. Must be in order [1 Rod, 2 S cone, 3 M cone]
+        spectralOverlap_Blue = [4.86e+18,5.57e+15,3.87e+18];%updated 7/1/21 - David
+        spectralOverlap_Green = [2.47e+18,2.65e+17,2.36e+18];%updated 7/1/21 - David
+        spectralOverlap_UV = 1.0e+18 *[1.283430135114683   0.435136715498093   1.197782733955319];%updated 8/8/22 Zach
+        
+        projectorColorMode = 'uv';
+        numberOfAmplifiers = 1;
+        
+        host = '192.168.0.3'; %What is the ip address to connect to the stage computer?  If Stage is running on this computer, use 'localhost'.
+        daq_type = 'NI'; %What brand data aquisition board is being used?  'Heka' or 'NI'
+
+        video_path = 'D:\Movies\'
+        
+        blankingCircuitComPort = 'COM3';
+    end
+    
+    methods
+        
+        function obj = SchwartzLab_Rig_B_2P(delayInit)
+            %{Port, bit number, unit} for any datastreams. 
+            % unit = 0 for unitless.  
+            % bit number = -1 for analog.
+            % Comment out if you don't want to use.
+     
+            obj.daqStreams('Oscilloscope Trigger') = {'doport0', 0, 0}; %
+            obj.daqStreams('Stim Time Recorder') = {'doport0', 1, 0}; %
+            obj.daqStreams('Frame timing') = {'ai3',-1, 'V'};
+            %daqStreams('Scanhead Trigger') = {'doport1', 2, 0}; %
+            %daqStreams('Optogenetics Trigger') = {'doport1', 3, 0}; %
+            %daqStreams('Scanhead Trigger') = {'doport1', 2, 0}; %
+            %daqStreams('Excitatory conductance') = {'ao2', -1, 'V'}; %
+            %daqStreams('Inhibitory conductance') = {'ao3', -1, 'V'}; %
+%             obj.daqStreams('LED_blanking_signal') = {'ai3', -1, 'V'}; %
+            
+            % obj.daqStreams('Test DI') = {'diport0', 7, 0};
+                       
+            if nargin < 1
+                delayInit = false;
+            end
+            
+            if ~delayInit
+                obj.initializeRig();
+            end
+        end
+
+    end
+    
+end
+
