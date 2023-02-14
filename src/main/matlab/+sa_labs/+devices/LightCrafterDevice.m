7cf1d2b1065d6f5f38924fc5eb1d58c9d3ab2ede
David <davidswygart@gmail.com>
2023-02-14 13:44:29 -0600

    correct canvas size?

--- src/main/matlab/+sa_labs/+devices/LightCrafterDevice.m
+++ src/main/matlab/+sa_labs/+devices/LightCrafterDevice.m
@@ -51,7 +51,8 @@ classdef LightCrafterDevice < symphonyui.core.Device
             obj.stageClient.setMonitorGamma(1);
             
             trueCanvasSize = obj.stageClient.getCanvasSize();
-            canvasSize = [trueCanvasSize(1) * 2, trueCanvasSize(2)];
+            % canvasSize = [trueCanvasSize(1) * 2, trueCanvasSize(2)]; %no idea why this was here
+            canvasSize = [trueCanvasSize(1), trueCanvasSize(2)];
             
             obj.stageClient.setCanvasProjectionIdentity();
             obj.stageClient.setCanvasProjectionOrthographic(0, canvasSize(1), 0, canvasSize(2));
