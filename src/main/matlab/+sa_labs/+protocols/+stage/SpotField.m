39f027f575b2ca56727ebbf9d600bef97d200156
davidswygart <davidswygart2015@u.northwestern.edu>
2023-11-06 10:23:04 -0600

    made a couple of new stimuli

---
+++ src/main/matlab/+sa_labs/+protocols/+stage/SpotField.m
@@ -0,0 +1,319 @@
+classdef SpotField < sa_labs.protocols.StageProtocol
+
+    properties
+        spotSize = 30
+
+        extentX = 60 %um
+        extentY = 60 %um
+
+        spotStimFrames = 15
+        spotPreFrames = 15
+        spotTailFrames = 45
+
+        spotIntensity = .5
+
+        gridMode = 'radial'
+        coverage = .9069
+
+        seed = -1                       % set to negative value to not use a seed, otherwise use a non-negative integer
+
+        numberOfFields = 20
+
+        spotLED
+    end
+    
+    properties (Hidden)
+        numSpotsPerEpoch = NaN;
+        
+        cx = [];
+        cy = [];
+        grid = [];
+
+        theta = [];
+
+        % responsePlotMode = 'cartesian';
+        % responsePlotSplitParameter = 'trialType';
+
+        randStream
+
+        responsePlotMode = false;
+
+        gridModeType = symphonyui.core.PropertyType('char', 'row', {'grid','random','rings','radial'});
+        
+        % nSpotsPresented = 0;
+    end
+    
+    properties (Dependent) 
+        stimTime
+        preTime
+        tailTime
+
+        RstarIntensitySpot
+        MstarIntensitySpot
+        SstarIntensitySpot
+
+    end
+    
+    properties (Hidden, Dependent)
+        totalNumEpochs
+    end
+    
+    methods
+        function obj = SpotField(obj)
+            obj@sa_labs.protocols.StageProtocol();
+            obj.colorPattern1 = 'uv';
+            obj.colorPattern2 = 'none';
+            obj.spotLED = obj.uvLED;
+        end
+
+
+        function d = getPropertyDescriptor(obj, name)
+            d = getPropertyDescriptor@sa_labs.protocols.StageProtocol(obj, name);
+            switch name
+            case 'spotLED'
+                d.category = '7 Projector';
+            case {'uvLED','redLED','greenLED','blueLED','RstarIntensity1','MstarIntensity1','SstarIntensity1'}
+                d.isHidden = true;
+            case {'coverage'}
+                if obj.gridMode
+                    d.isHidden = false;
+                else
+                    d.isHidden = true;
+                end
+            case {'RstarIntensitySpot','MstarIntensitySpot','SstarIntensitySpot'}
+                d.category = '6 Isomerizations';
+            end
+
+        end
+        
+        function prepareRun(obj)
+            prepareRun@sa_labs.protocols.StageProtocol(obj);
+
+            dt = 1/obj.frameRate; % assume frame rate in Hz
+            
+            % *0.001 is to make in terms of seconds
+
+            obj.theta = linspace(0,2*pi,11);
+            obj.theta(end) = [];
+            
+            obj.numSpotsPerEpoch = floor(35 * obj.frameRate / (obj.spotPreFrames + obj.spotStimFrames + obj.spotTailFrames));
+
+            if strcmp(obj.gridMode,'grid')
+                %space the spots to achieve the desired coverage factor
+                %uses the ratio of the area of a hexagon to that of a circle
+                spaceFactor = sqrt(3*pi/4 / obj.coverage / (3*sqrt(3)/2));
+                spacing = spaceFactor * obj.spotSize;
+    
+                %find the x and y coordinates for the hex grid
+                xa = [0:-spacing:-obj.extentX/2-spacing, spacing:spacing:obj.extentX/2+spacing];
+                xb= [xa - spacing/2, xa(end)+spacing/2];
+                yspacing = cos(pi/6)*spacing;
+                ya = [0:-2*yspacing:-obj.extentY/2-yspacing, 2*yspacing:2*yspacing:obj.extentY/2+yspacing];
+                yb = [ya - yspacing, ya(end) + yspacing];
+    
+                %create the grid
+                [xqa, yqa] = meshgrid(xa,ya);
+                [xqb, yqb] = meshgrid(xb,yb);
+                locs = [xqa(:), yqa(:); xqb(:), yqb(:)];
+    
+                halfGrids = [obj.extentX, obj.extentY]/2;
+                %remove any circles that don't intersect the grid rectangle
+                % 1) the bounding box of the circle must intersect the rectangle
+                locs = locs(all(abs(locs) < repmat(halfGrids, size(locs,1),1) + obj.spotSize/2, 2), :);
+    
+                % 2) circles near the corners might have an intersecting
+                %       bounding box but not actually intersect
+                % - if either of the coordinates is inside the box, it
+                %       definitely intersects
+                % - otherwise it must intersect the corner
+                halfGrids = repmat(halfGrids, size(locs,1),1);
+                obj.grid = locs(any(abs(locs) < halfGrids, 2) | 4*sum((abs(locs)-halfGrids).^2,2) <= obj.spotSize.^2 , :);
+                
+                if obj.numSpotsPerEpoch > size(obj.grid,1)
+                    obj.grid = repmat(obj.grid, ceil(obj.numSpotsPerEpoch / size(obj.grid,1)), 1);
+                end
+            elseif strcmp(obj.gridMode,'rings')
+                N = cumsum([6, 10, 14, 8, 16, 30]);
+                R = [8, 9, 12, 22, 24, 28] / 60; % * obj.extentX;
+                obj.grid = [];
+                for i = 1:numel(N)
+                    th = linspace(0, 2*pi, N(i) + 1)';
+                    obj.grid = vertcat(obj.grid, R(i)*[cos(th(1:end-1)), sin(th(1:end-1))]);
+                end
+                obj.grid(:,1) = obj.grid(:,1) * obj.extentX;
+                obj.grid(:,2) = obj.grid(:,2) * obj.extentY;
+            elseif strcmp(obj.gridMode,'radial') 
+                   %TODO radial part
+                   N = cumsum([6, 10, 14, 8, 16, 30]);
+                   R = [8, 9, 12, 22, 24, 28] / 60; % * obj.extentX;
+                   obj.grid = [];
+                   for i = 1:numel(N)
+                       th = linspace(0, 2*pi, N(i) + 1)';
+                       obj.grid = vertcat(obj.grid, R(i)*[cos(th(1:end-1)), sin(th(1:end-1))]);
+                   end
+                   obj.grid(:,1) = obj.grid(:,1) * obj.extentX;
+                   obj.grid(:,2) = obj.grid(:,2) * obj.extentY;
+            end
+
+            if obj.seed >= 0
+                obj.randStream = RandStream('mt19937ar','seed',obj.seed);
+            else
+                obj.randStream = RandStream.getGlobalStream();
+            end
+
+            devices = {};
+            modes = {};
+            for ci = 1:4
+                ampName = obj.(['chan' num2str(ci)]);
+                ampMode = obj.(['chan' num2str(ci) 'Mode']);
+                if ~(strcmp(ampName, 'None') || strcmp(ampMode, 'Off'))
+                    device = obj.rig.getDevice(ampName);
+                    devices{end+1} = device; %#ok<AGROW>
+                    modes{end+1} = ampMode;
+                end
+            end
+
+            obj.responseFigure = obj.showFigure('sa_labs.figures.SpotsMultiLocationFigure', devices, modes, ...
+                    'totalNumEpochs', obj.totalNumEpochs,...
+                    'preTime', obj.spotPreFrames / obj.frameRate,...
+                    'stimTime', obj.spotStimFrames / obj.frameRate,...
+                    'tailTime', obj.spotTailFrames / obj.frameRate,...
+                    'spotsPerEpoch', obj.numSpotsPerEpoch, ...
+                    'spikeThreshold', obj.spikeThreshold, 'spikeDetectorMode', obj.spikeDetectorMode);
+        end
+        
+        function prepareEpoch(obj, epoch)
+            if strcmp(obj.gridMode,'random')
+                obj.cx = rand(obj.randStream, obj.numSpotsPerEpoch, 1) * obj.extentX - obj.extentX/2;
+                obj.cy = rand(obj.randStream, obj.numSpotsPerEpoch, 1) * obj.extentY - obj.extentY/2;
+            else
+                spots = randperm(obj.randStream, size(obj.grid,1), obj.numSpotsPerEpoch);
+                %would be better to do a complete permutation...
+                obj.cx = obj.grid(spots,1);
+                obj.cy = obj.grid(spots,2);
+            end
+            
+            epoch.addParameter('cx', obj.cx);
+            epoch.addParameter('cy', obj.cy);
+
+            % Call the base method.
+            prepareEpoch@sa_labs.protocols.StageProtocol(obj, epoch);
+        end
+        
+        function controllerDidStartHardware(obj)
+            lightCrafter = obj.rig.getDevice('LightCrafter');
+            LED = obj.spotLED;
+            lightCrafter.setLedCurrents(0, 0, 0, LED);
+            controllerDidStartHardware@sa_labs.protocols.StageProtocol(obj);
+        end
+
+        function completeRun(obj)
+            lightCrafter = obj.rig.getDevice('LightCrafter');
+            lightCrafter.setLedCurrents(0, 0, 0, obj.spotLED); %final level is deterministic
+            completeRun@sa_labs.protocols.StageProtocol(obj);
+        end
+
+        function p = createPresentation(obj)
+            
+            canvasSize = reshape(obj.rig.getDevice('Stage').getCanvasSize(),2,1);
+            [~,cx_] = obj.um2pix(obj.cx);
+            [~,cy_] = obj.um2pix(obj.cy);
+                       
+            spotPre = obj.spotPreFrames;
+            spotPreStim = obj.spotPreFrames+ obj.spotStimFrames;
+            spotPreStimPost = obj.spotPreFrames+ obj.spotStimFrames + obj.spotTailFrames;
+            function xy = getSpotPosition(state)
+                i = min(floor(state.frame / spotPreStimPost) + 1, length(cx_));
+                % i = min(mod(state.frame, obj.spotPreFrames+ obj.spotStimFrames + obj.spotTailFrames) + 1, length(obj.cx));
+                
+                % canvasSize / 2 + self.um2pix(self.currSpot(1:2));
+                xy = canvasSize/2 + [cx_(i); cy_(i)];
+            end
+            
+            sI = obj.spotIntensity;
+            function c = getSpotIntensity(state)
+                if state.frame >= (nFrames - 1)
+                    c = 0;
+                    return
+                end
+                
+                i = mod(state.frame, spotPreStimPost);
+
+                if (i < spotPre) || (i >= spotPreStim)
+                    c = 0;
+                else
+                    c = sI;
+                end
+            end
+            
+            p = stage.core.Presentation(35);
+
+                spot = stage.builtin.stimuli.Ellipse();
+            
+                [~,spot.radiusX] = obj.um2pix(obj.spotSize / 2);
+                spot.radiusY = spot.radiusX;
+                spot.opacity = 1;
+                spot.color = 0;
+                
+                spotIntensity_ = stage.builtin.controllers.PropertyController(spot, 'color',...
+                    @(state)getSpotIntensity(state));
+                spotPosition = stage.builtin.controllers.PropertyController(spot, 'position',...
+                    @(state)getSpotPosition(state));
+                
+                p.addStimulus(spot);
+
+                p.addController(spotIntensity_);
+                p.addController(spotPosition);
+
+        end
+
+        function preTime = get.preTime(~)
+            % if strcmp(obj.trialType,'chirp')
+            %     preTime = 2000;
+            % else
+                preTime = 0;
+            % end
+        end
+        
+        function tailTime = get.tailTime(~)
+            % if strcmp(obj.trialType,'chirp')
+            %     tailTime = 2000;
+            % else
+                tailTime = 0;
+            % end
+        end
+
+        function stimTime = get.stimTime(~)
+            
+            % if strcmp(obj.trialType,'chirp')
+                stimTime = 5000;
+            % else
+            %     stimTime = obj.spotCountInX * obj.spotCountInY * (obj.spotPreFrames+ obj.spotStimFrames + obj.spotTailFrames) / obj.frameRate * 1e3;
+            % end
+        end
+        
+        function totalNumEpochs = get.totalNumEpochs(obj)
+            totalNumEpochs = obj.numberOfFields;
+        end
+        
+        function RstarIntensity = get.RstarIntensitySpot(obj)
+            params = obj.isomerizationParameters();
+            params.uvLED = obj.spotLED;
+            [RstarIntensity, ~, ~] = obj.convertIntensityToIsomerizations(obj.spotIntensity, 'uv', params);
+        end
+        
+        function MstarIntensity = get.MstarIntensitySpot(obj)
+            params = obj.isomerizationParameters();
+            params.uvLED = obj.spotLED;
+            [~, MstarIntensity, ~] = obj.convertIntensityToIsomerizations(obj.spotIntensity, 'uv', params);
+        end
+        
+        function SstarIntensity = get.SstarIntensitySpot(obj)
+            params = obj.isomerizationParameters();
+            params.uvLED = obj.spotLED;
+            [~, ~, SstarIntensity] = obj.convertIntensityToIsomerizations(obj.spotIntensity, 'uv', params);
+        end
+
+    end
+    
+end
---
+++ src/main/matlab/+sa_labs/+rigs/SchwartzLab_Rig_A_Opto.m
@@ -0,0 +1,71 @@
+classdef SchwartzLab_Rig_A_Opto < sa_labs.rigs.SchwartzLab_Rig_Base
+    
+    properties
+        % properties not accessible here; have to be fed into a device to work
+        rigName = 'Schwartz Lab Rig A Opto';
+        testMode = false;
+        filterWheelNdfValues = [1, 2, 3, 4, 5, 0];
+        filterWheelDefaultValue = 5;
+        
+        filterWheelAttenuationValues_Blue = [1.58e-1, 11.2e-3, 9.01e-4, 8.23e-5, 6.96e-6, 1.582];%updated 6/1/2022 -David
+        filterWheelAttenuationValues_Green = [1e-1, 1e-2 , 1e-3 , 1.1e-4 , 1.1e-5, 1];%updated 5/26/2022 -David
+        filterWheelAttenuationValues_UV = [5.8e-02, 1.7e-03, 1.2e-04, 0.6e-5, 0.3e-6, 1];%updated 11/15/2022 -David
+        
+        fitBlue = [9.10317387691189e-18	-1.18865973635156e-14	3.85486264901338e-12	-1.66847157811571e-11];%updated 5/26/2022 -David
+        fitGreen =[1.21309650979622e-18	-4.08598841738357e-16	3.78474579983637e-14	-4.17594216385694e-14];%updated 5/26/2022 -David (Green projector not modulating current and is dim)
+        fitUV =   [-1.03e-18, -1.07e-16, 1.89e-13, 1.76e-11];%updated 11/15/2022 -David
+        
+        micronsPerPixel = 1.3; %updated 6/1/2022 -David
+        frameTrackerPosition = [1724,50]; %updated 5/26/2022 -David
+        frameTrackerSize = [200,100]; %updated 5/26/2022 -David
+        frameTrackerBackgroundSize = [200, 100]; %updated 7/28/2022 - Zach
+        
+        filterWheelComPort = 'COM7';
+        orientation = [false, true];%[flip Y, flip X]
+        angleOffset = 0; %Does not actually change presentation.  Is saved in epoch data so it could be used in analysis, but it isn't used now.
+        
+        %Overlap of the Rod, S_cone, and M_cone spectrum with each LED. Must be in order [1 Rod, 2 S cone, 3 M cone]
+        spectralOverlap_Blue = [4.76558684709051e+18	6.55091000309801e+15	3.81318946776386e+18];%updated 5/26/2022 -David
+        spectralOverlap_Green = [2.66406358405798e+18	1.10188075264679e+15	3.77493986028575e+18];%updated 5/26/2022 -David
+        spectralOverlap_UV = [9.68180413015343e+17	1.31761176294673e+18	1.12668172298288e+18];%updated 5/26/2022 -David
+        
+        projectorColorMode = 'uv2'; % Rig A has MkII projector
+        numberOfAmplifiers = 2;
+        
+        host = '192.168.0.3'; %What is the ip address to connect to the stage computer?  If Stage is running on this computer, use 'localhost'.
+        daq_type = 'NI'; %What brand data aquisition board is being used?  'Heka' or 'NI'
+    end
+    
+    methods
+        
+        function obj = SchwartzLab_Rig_A_Opto(delayInit)
+            %{Port, bit number, unit} for any datastreams. 
+            % unit = 0 for unitless.  
+            % bit number = -1 for analog.
+            % Comment out if you don't want to use.
+     
+            obj.daqStreams('Oscilloscope Trigger') = {'doport0', 0, 0}; %
+            obj.daqStreams('Stim Time Recorder') = {'doport0', 1, 0}; %
+            obj.daqStreams('Optogenetics Trigger') = {'doport0', 6, 0}; %
+%             obj.daqStreams('Scanhead Trigger') = {'doport1', 2, 0}; %
+%             obj.daqStreams('ScanImageShutter') = {'diport0', 2, 0};
+            obj.daqStreams('Bath Temperature') = {'ai2',-1,'degC'};
+            obj.daqStreams('Bath Temperature Control') = {'ai3',-1,'degC'};
+            
+            % Sophia changes 12/14/21
+%             obj.daqStreams('Excitatory conductance') = {'ao2', -1, 'V'}; %
+%             obj.daqStreams('Inhibitory conductance') = {'ao3', -1, 'V'}; %
+            
+            if nargin < 1
+                delayInit = false;
+            end
+            
+            if ~delayInit
+                obj.initializeRig();
+            end
+        end
+        
+    end
+    
+end
+
