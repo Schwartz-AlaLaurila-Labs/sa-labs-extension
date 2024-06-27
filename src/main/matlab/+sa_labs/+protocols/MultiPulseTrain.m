e3fa9b1c87aaa6c0bdccf35257cf1d8c25867920
Schwartz Lab <z.jessen@gmail.com>
2024-06-27 17:24:36 -0500

    not working multipulsetrain

--- src/main/matlab/+sa_labs/+protocols/MultiPulseTrain.m
+++ src/main/matlab/+sa_labs/+protocols/MultiPulseTrain.m
@@ -6,12 +6,11 @@ classdef MultiPulseTrain < sa_labs.protocols.BaseProtocol
     properties
         outputAmpSelection = 1              % Output amplifier (1 or 2)
         preTime = 500                       % Pulse leading duration (ms)
-        SpikeTrainpulseTime = 2                       % Duration of each individual pulse (ms)
-        testpulsetime = 50
-        TestPulseAmplitude = 50
+        SpikeTrainpulseTime = 2             % Duration of each individual pulse (ms)
+        testpulsetime = 50                  % Duration of each individual test pulse(ms)
+        TestPulseAmplitude = 50             % Test pulse amplitude (pA)
         tailTime = 500                    % Pulse trailing duration (ms)
         STFreq = 25                         % Stimulation frequency (Hz)
-        PTFreq = 0.5                         % Test pulses frequency (Hz)
         pulseAmplitude = 1000               % Pulse amplitude (mV or pA depending on amp mode)
         PulseTrainTime = 250                   % Pulse train duration (ms)
         numberOfEpochs = 10;
@@ -19,24 +18,26 @@ classdef MultiPulseTrain < sa_labs.protocols.BaseProtocol
     
     properties (Dependent)
         numPulses % Total number of current/voltage train pulses
+        STnumPulses
         stimTime
     end
     
     properties (Hidden)
         responsePlotMode = 'cartesian';
         responsePlotSplitParameter = ''; %'pulseAmplitude';
-        stim1Time
-        stim2Time
         test1numPulses = 4
         test2numPulses = 9
+        PTFreq = 0.5                         % Test pulses frequency (Hz)
     end
     
     properties (Hidden, Dependent)
         totalNumEpochs
-        pulseInterval = 1000/obj.STFreq;      %time from the start of one pulse to the start of the next pulse (ms)
-        intervalTime = pulseInterval - obj.SpikeTrainpulseTime; %Inter-pulse interval duration (ms)
-        TestPulseInterval = 1000/obj.PTFreq;
-        TestPulseIntervalTime = TestPulseInterval - obj.testpulsetime;
+        pulseInterval      %time from the start of one pulse to the start of the next pulse (ms)
+        intervalTime %Inter-pulse interval duration (ms)
+        TestPulseInterval 
+        TestPulseIntervalTime
+        stim1Time
+        stim2Time
     end    
     
     methods
@@ -50,29 +51,27 @@ classdef MultiPulseTrain < sa_labs.protocols.BaseProtocol
         function stim = createAmpStimulus(obj, ampName)
 
             stimCell = {};
-            stim1Time = test1numPulses*(1000/obj.PTFreq)
-            stim2Time = test2numPulses*(1000/obj.PTFreq)
             
-            % create delay pulse
-%             % create background pulse
-%             genB = symphonyui.builtin.stimuli.PulseGenerator();
-%             
-%             genB.preTime = 0;
-%             genB.stimTime = obj.preTime+obj.stim1Time+ obj.PulseTrainTime + obj.stim2Time+obj.tailTime;
-%             genB.tailTime = 0;
-%             genB.amplitude = obj.rig.getDevice(ampName).background.quantity;
-%             genB.mean = 0;
-%             genB.sampleRate = obj.sampleRate;
-%             genB.units = obj.rig.getDevice(ampName).background.displayUnits;
-% 
-%             stimCell{1} = genB.generate();
-%             
+            %create delay pulse
+            % create background pulse
+            genB = symphonyui.builtin.stimuli.PulseGenerator();
+            
+            genB.preTime = 0;
+            genB.stimTime = obj.preTime + obj.stim1Time + obj.PulseTrainTime + obj.stim2Time + obj.tailTime;
+            genB.tailTime = 0;
+            genB.amplitude = obj.rig.getDevice(ampName).background.quantity;
+            genB.mean = 0;
+            genB.sampleRate = obj.sampleRate;
+            genB.units = obj.rig.getDevice(ampName).background.displayUnits;
+
+            stimCell{1} = genB.generate();
+            
             %Create stim baseline pulses
             gen1 = symphonyui.builtin.stimuli.PulseTrainGenerator();
             
             gen1.preTime = obj.preTime;
             gen1.pulseTime = obj.testpulsetime;
-            gen1.tailTime = (obj.PTFreq/2)*1000;  %Test pulses frequency over 2 (ms)
+            gen1.tailTime =  obj.PulseTrainTime + obj.stim2Time+obj.tailTime;  %Test pulses frequency over 2 (ms)
             gen1.intervalTime = obj.TestPulseIntervalTime;
             gen1.amplitude = obj.TestPulseAmplitude;
             gen1.numPulses = obj.test1numPulses;
@@ -80,7 +79,7 @@ classdef MultiPulseTrain < sa_labs.protocols.BaseProtocol
             gen1.sampleRate = obj.sampleRate;
             gen1.units = obj.rig.getDevice(ampName).background.displayUnits;
             
-            stimCell{1} = gen1.generate();
+            stimCell{2} = gen1.generate();
 
             %Create spike train
 
@@ -88,21 +87,21 @@ classdef MultiPulseTrain < sa_labs.protocols.BaseProtocol
             
             gen2.preTime = obj.preTime + obj.stim1Time;
             gen2.pulseTime = obj.SpikeTrainpulseTime;
-            gen2.tailTime = (obj.PTFreq/2)*1000;
+            gen2.tailTime = (obj.PTFreq/2)*1000; %incorrect
             gen2.intervalTime = obj.intervalTime;
             gen2.amplitude = obj.TestPulseAmplitude;
-            gen2.numPulses = obj.test1numPulses;
+            gen2.numPulses = obj.STnumPulses;
             gen2.mean = 0; %obj.rig.getDevice(ampName).background.quantity;
             gen2.sampleRate = obj.sampleRate;
             gen2.units = obj.rig.getDevice(ampName).background.displayUnits;
             
-            stimCell{2} = gen2.generate();
+            stimCell{3} = gen2.generate();
 
             %Create PostTrain pulses
 
             gen3 = symphonyui.builtin.stimuli.PulseTrainGenerator();
             
-            gen3.preTime = obj.preTime + obj.stim1Time + obj.stim2Time;
+            gen3.preTime = obj.preTime + obj.stim1Time +  obj.PulseTrainTime;
             gen3.pulseTime = obj.testpulsetime;
             gen3.tailTime = obj.tailTime;  %Test pulses frequency over 2 (ms)
             gen3.intervalTime = obj.TestPulseIntervalTime;
@@ -112,7 +111,7 @@ classdef MultiPulseTrain < sa_labs.protocols.BaseProtocol
             gen3.sampleRate = obj.sampleRate;
             gen3.units = obj.rig.getDevice(ampName).background.displayUnits;
             
-            stimCell{3} = gen3.generate();
+            stimCell{4} = gen3.generate();
 
             % add together all stimuli
             genSum = symphonyui.builtin.stimuli.SumGenerator();
@@ -135,18 +134,45 @@ classdef MultiPulseTrain < sa_labs.protocols.BaseProtocol
             epoch.addStimulus(obj.rig.getDevice(outputAmpName), obj.createAmpStimulus(outputAmpName));
         end
         
+        %Define dependent properties and how they are calculated
         
         function totalNumEpochs = get.totalNumEpochs(obj)
             totalNumEpochs = obj.numberOfEpochs;
         end        
-        
-        function numPulses = get.numPulses(obj)
-            numPulses = obj.stimTime/(1000/obj.STFreq); 
+        function STnumPulses = get.STnumPulses(obj)
+            STnumPulses = obj.PulseTrainTime/(1000/obj.STFreq);
         end
+        function numPulses = get.numPulses(obj)
+            numPulses = obj.test1numPulses + obj.test2numPulses + obj.STnumPulses; 
+        end
+        
         function stimTime = get.stimTime(obj)
             stimTime = obj.stim1Time+obj.PulseTrainTime+obj.stim2Time;
-        end 
+        end
         
+        function stim1Time = get.stim1Time(obj) 
+            stim1Time = obj.test1numPulses*(1000/obj.PTFreq); %this is wrong
+        end
+        
+        function stim2Time = get.stim2Time(obj)
+            stim2Time = obj.test2numPulses*(1000/obj.PTFreq); %this is wrong
+        end
+        
+        function intervalTime = get.intervalTime(obj)
+            intervalTime = obj.pulseInterval - obj.SpikeTrainpulseTime; %Inter-pulse interval duration (ms)
+        end
+        
+        function TestPulseInterval = get.TestPulseInterval(obj)
+            TestPulseInterval = 1000/obj.PTFreq;
+        end
+        
+        function TestPulseIntervalTime = get.TestPulseIntervalTime(obj)
+            TestPulseIntervalTime = obj.TestPulseInterval - obj.testpulsetime;
+        end
+        
+        function pulseInterval = get.pulseInterval(obj)
+            pulseInterval = 1000/obj.STFreq;
+        end
     end
     
 end
