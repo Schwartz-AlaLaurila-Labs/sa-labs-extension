350bf5bbb29eaf7993509a5e314e7fced04bfc5e
Schwartz Lab <z.jessen@gmail.com>
2025-05-28 15:38:51 -0500

    Update BrainCell.m

--- src/main/matlab/+sa_labs/+sources/+brain/BrainCell.m
+++ src/main/matlab/+sa_labs/+sources/+brain/BrainCell.m
@@ -14,7 +14,7 @@ classdef BrainCell < symphonyui.core.persistent.descriptions.SourceDescription
             obj.addProperty('brain_region', '', ...
                 'description', 'Plain text (for now) about the brain region ');
 
-            obj.addAllowableParentType('sa_labs.sources.BrainSlice');
+            obj.addAllowableParentType('sa_labs.sources.brain.BrainSlice');
         end
         
     end
