b1bc35c61108eab345fa7caf90135a5971d467ae
Schwartz Lab <z.jessen@gmail.com>
2025-11-03 15:57:14 -0600

    trying to change the brain region to actual DJ entries
    XZ

--- src/main/matlab/+sa_labs/+sources/+brain/BrainCell.m
+++ src/main/matlab/+sa_labs/+sources/+brain/BrainCell.m
@@ -4,6 +4,9 @@ classdef BrainCell < symphonyui.core.persistent.descriptions.SourceDescription
         
         function obj = BrainCell(parent)
             import symphonyui.core.*;
+
+            %fetch the acronym of the current brain regions
+            all_regions = fetchn(sln_animal.BrainArea, 'target');
                        
             obj.addProperty('number',uint8(0),'type',PropertyType('uint8','scalar'),...
                 'description', 'The number of the cell (just the number)')
@@ -11,8 +14,13 @@ classdef BrainCell < symphonyui.core.persistent.descriptions.SourceDescription
             obj.addProperty('notes', '', ...
                 'description', 'Notes about this recording');
                   
-            obj.addProperty('brain_region', '', ...
-                'description', 'Plain text (for now) about the brain region ');
+            %obj.addProperty('brain_region', '', ...
+             %   'description', 'Plain text (for now) about the brain region ');
+            
+            %Getting brain region acronym from table sln_animal.BrainArea for selection
+            obj.addProperty('brain_region', '', 'description', ...
+                'acronym of the region', 'type', ...
+                PropertyType('char', 'row', [{''}; all_regions]));
 
             obj.addAllowableParentType('sa_labs.sources.brain.BrainSlice');
         end
